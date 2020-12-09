d8c8327e03bed6dafe1ccfc87c67de468a99a022
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-09 19:08:48 +0100

    Update UserAdmin.php

--- code/Model/UserAdmin.php
+++ code/Model/UserAdmin.php
@@ -161,13 +161,13 @@ class UserAdmin extends User
 
 }
 
-//$admin = new UserAdmin();
-//$admin->identification('admin','12345');
+$admin = new UserAdmin();
+$admin->identification('admin','12345');
 
-//$admin->loadUser();
-//$admin->createEquipment('AX151','Smartphone','Iphone','9','9.0',6);
-//$admin->createEquipment('XX157','Smartphone','HUWEI','11','15.0',5);
-//$admin->createEquipment('XX283','Smartphone','HUWEI','11','15.0',5);
+
+$admin->createEquipment('AX151','Smartphone','Iphone','9','9.0',6);
+$admin->createEquipment('XX157','Smartphone','HUWEI','11','15.0',5);
+$admin->createEquipment('XX283','Smartphone','HUWEI','11','15.0',5);
 //$admin->borrowEquipement('XX157','2021/12/05',1);
 //$admin->borrowEquipement('AX156','2021/07/15',1);
 //$admin->borrowEquipement('AX156','2021/11/12',1);
