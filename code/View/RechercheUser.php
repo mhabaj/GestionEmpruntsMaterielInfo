c10a62956ac992aa4bd1c80417063a2bba810159
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-23 16:07:28 +0100

    add rechercheUser

---
+++ code/View/RechercheUser.php
@@ -0,0 +1,59 @@
+<?php
+require_once("Controller/control-session.php");
+
+require "Controller/DataBase.php";
+
+?>
+<html>
+<body>
+<form method="POST" enctype="multipart/form-data">
+    <h1>Recherche utilisateur</h1>
+    <label>Rechercher utilisateur:</label>
+    <input type="search" placeholder="Inserer le nom de l'utilisateur" name="UserToSearch">
+    <button type="submit" name="startSearching">Rechercher</button>
+</form>
+
+<?php
+
+if (isset($_GET['type'])) {
+    $bdd = new DataBase();
+    $con = $bdd->getCon();
+
+    $queryEquipments = "SELECT * FROM equipment WHERE type_equip LIKE ?;";
+    $myStatement = $con->prepare($queryEquipments);
+    $myStatement->execute([$_GET['type']]);
+
+    while ($donnees = $myStatement->fetch()) { ?>
+        <a href="DetailEquipement.php?ref_equip=<?php echo $donnees['ref_equip'] ?>">
+            <div>
+                <strong> Type </strong> : <?php echo $donnees['type_equip']; ?> <br/>
+                <strong> Matériel </strong> : <?php echo $donnees['brand_equip'] . " " . $donnees['name_equip']; ?>
+                <br/>
+                <strong> Version </strong> : <?php echo $donnees['version_equip']; ?> <br/> <br/>
+            </div>
+        </a>
+        <?php
+    }
+    $myStatement->closeCursor();
+} else {
+    $bdd = new DataBase();
+    $con = $bdd->getCon();
+
+    $queryEquipments = "SELECT DISTINCT(type_equip) FROM equipment ;";
+    $myStatement = $con->query($queryEquipments);
+
+    while ($donnees = $myStatement->fetch()) { ?>
+        <a href="Catalogue.php?type=<?php echo $donnees['type_equip'] ?>">
+            <div>
+                <strong> Type </strong> : <?php echo $donnees['type_equip']; ?> <br/>
+            </div>
+        </a>
+        <?php
+    }
+    $myStatement->closeCursor();
+
+}
+?>
+
+</body>
+</html>
