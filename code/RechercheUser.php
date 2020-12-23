881ece13c33babcc5d4fb0217cae5f73ad2501ef
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-23 19:23:04 +0100

    update

---
+++ code/RechercheUser.php
@@ -0,0 +1,41 @@
+<?php
+require_once("Controller/control-session.php");
+
+require "Controller/DataBase.php";
+
+?>
+<html>
+<body>
+<form method="POST" enctype="multipart/form-data">
+    <h1>Recherche utilisateur</h1>
+    <label>Rechercher utilisateur:</label>
+    <input type="search" placeholder="Inserer le matricule de l'utilisateur" name="UserToSearch">
+    <button type="submit" name="startSearching">Rechercher</button>
+</form>
+<?php
+
+if (isset($_POST['startSearching'])) {
+    $bdd = new DataBase();
+    $con = $bdd->getCon();
+
+    $queryEquipments = "SELECT * FROM users WHERE matricule_user LIKE ?;";
+    $myStatement = $con->prepare($queryEquipments);
+    $myStatement->execute([$_POST['UserToSearch']]);
+
+    while ($donnees = $myStatement->fetch()) { ?>
+        <a href="https://youtu.be/rrNTRqf-Nqs">
+            <div>
+                <strong> Firstname </strong> : <?php echo $donnees['name_user']; ?> <br/>
+                <strong> Lastname </strong> : <?php echo $donnees['lastname_user']; ?> <br/>
+                <strong> email </strong> : <?php echo $donnees['email_user']; ?> <br/> <br/>
+            </div>
+        </a>
+        <?php
+    }
+    $myStatement->closeCursor();
+
+}
+?>
+
+</body>
+</html>
