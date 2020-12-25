355edc774722c026e38f6f373e59735347555c6f
Adrien <adrien.ingand@gmail.com>
2020-12-25 23:51:34 +0100

    Update Functions.php

--- code/Controller/Functions.php
+++ code/Controller/Functions.php
@@ -10,7 +10,7 @@ class Functions
         }
         else
         {
-            throw new Exception("Incorrect refEquip input");
+            throw new Exception("La reference que vous avez entré est invalide");
         }
     }
 
@@ -22,7 +22,7 @@ class Functions
         }
         else
         {
-            throw new Exception("Incorrect mail input");
+            throw new Exception("Le mail que vous avez entré est invalide");
         }
     }
 
@@ -34,7 +34,7 @@ class Functions
         }
         else
         {
-            throw new Exception("Incorrect number input");
+            throw new Exception("Le numéro de telephone que vous avez entré est invalide");
         }
     }
 
@@ -46,31 +46,31 @@ class Functions
         }
         else
         {
-            throw new Exception("Incorrect matricule input");
+            throw new Exception("Votre identifiant de connexion est invalide");
         }
     }
 
     public static function checkNameMateriel($nom)
     {
-        if (preg_match('/^([A-Z]|[a-z]|[0-9]){1,30}$/', $nom))
+        if (preg_match('/[A-Za-z0-9]{1,30}/', $nom))
         {
             return true;
         }
         else
         {
-            throw new Exception("Incorrect nom materiel input");
+            throw new Exception("Le nom du matériel que vous avez entré est invalide");
         }
     }
 
     public static function checkVersionMateriel($version)
     {
-        if (preg_match('/([A-Z]|[a-z]|[0-9.-]|[,;:#"]){3,15}/', $version))
+        if (preg_match('/^[0-9.-_,;:#"]{3,15}$/', $version))
         {
             return true;
         }
         else
         {
-            throw new Exception("Incorrect nom materiel input");
+            throw new Exception("La version du matériel que vous avez entré est invalide");
         }
     }
 
@@ -82,7 +82,7 @@ class Functions
         }
         else
         {
-            throw new Exception("Incorrect nom utilisateur input");
+            throw new Exception("Le Nom que vous avez entré est invalide");
         }
     }
 
@@ -94,9 +94,33 @@ class Functions
         }
         else
         {
-            throw new Exception("Incorrect nom utilisateur input");
+            throw new Exception("Le prénom que vous avez entré est invalide");
+        }
+    }
+
+    public static function checkBrandName($brand)
+    {
+        if (preg_match('/^([A-Za-z0-9-_, ]){1,30}$/', $brand))
+        {
+            return true;
+        }
+        else
+        {
+            throw new Exception("Le nom de la marque que vous avez entré est invalide");
+        }
+    }
+
+    public static function checkTypeName($type)
+    {
+        if (preg_match('/^([A-Za-z0-9-_, ]){1,30}$/', $type))
+        {
+            return true;
+        }
+        else
+        {
+            throw new Exception("Le type de la marque que vous avez entré est invalide");
         }
     }
 }
 
-Functions::checkNameUser('david');
+Functions::checkBrandName('caudàlie');
