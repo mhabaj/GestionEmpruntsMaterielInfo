2127856e7b796c5b8b7a768608cddd61b89df65f
ALHABAJ Mahmod <mhabaj99@hotmail.com>
2020-12-26 19:53:35 +0200

    GROS MERGE AVEC ADRIEN

--- code/Catalogue.php
+++ code/Catalogue.php
@@ -7,64 +7,61 @@ require_once "Model/UserRegular.php";
 require_once "Model/UserAdmin.php";
 require_once "Controller/Functions.php";
 require_once "Controller/EquipmentController.php";
-
-class CatalogueController{
-
-}
-
+require_once "Controller/CatalogueController.php";
 
 ?>
-
 <html>
 <body>
 <form method="POST" enctype="multipart/form-data">
     <h1>Catalogue</h1>
     <label>Rechercher équipement:</label>
     <input type="search" placeholder="Inserer l'équipement" name="EquipmentToSearch">
+    <input type="radio" name="radio_recherche" id="radio_name" value="radio_name" checked>
+    <label for="radio_name">par nom</label>
+    <input type="radio" name="radio_recherche" id="radio_ref" value="radio_ref">
+    <label for="radio_ref">par référence</label>
     <button type="submit" name="startSearching">Rechercher</button>
 </form>
 
 <?php
-
-if (isset($_GET['type'])) {
+if (isset($_POST['startSearching']) && $_POST['EquipmentToSearch'] != null && $_POST['EquipmentToSearch'] != " ") {
     $bdd = new DataBase();
     $con = $bdd->getCon();
+    $EquipToSearch = trim($_POST['EquipmentToSearch']);
 
-    $queryEquipments = "SELECT * FROM equipment WHERE type_equip LIKE ?;";
+    if ($_POST['radio_recherche'] == "radio_name")
+        $queryEquipments = "SELECT * FROM equipment WHERE name_equip LIKE ?;";
+    elseif ($_POST['radio_recherche'] == "radio_ref")
+        $queryEquipments = "SELECT * FROM equipment WHERE ref_equip LIKE ?;";
+
     $myStatement = $con->prepare($queryEquipments);
-    $myStatement->execute([$_GET['type']]);
+    $myStatement->execute([$EquipToSearch . "%"]);
 
-    while ($donnees = $myStatement->fetch()) { ?>
-        <a href="DetailEquipement.php?ref_equip=<?php echo $donnees['ref_equip'] ?>">
-            <div>
-                <strong> Type </strong> : <?php echo $donnees['type_equip']; ?> <br/>
-                <strong> Matériel </strong> : <?php echo $donnees['brand_equip'] . " " . $donnees['name_equip']; ?>
-                <br/>
-                <strong> Version </strong> : <?php echo $donnees['version_equip']; ?> <br/> <br/>
-            </div>
-        </a>
+    if ($myStatement->rowCount() > 0) {
+        while ($donnees = $myStatement->fetch()) { ?>
+            <a href="DetailEquipement.php?ref_equip=<?php echo $donnees['ref_equip'] ?>">
+                <div>
+                    <strong> Type </strong> : <?php echo $donnees['type_equip']; ?> <br/>
+                    <strong> Matériel </strong> : <?php echo $donnees['brand_equip'] . " " . $donnees['name_equip']; ?>
+                    <br/>
+                    <strong> Version </strong> : <?php echo $donnees['version_equip']; ?> <br/> <br/>
+                </div>
+            </a>
+            <?php
+        }
+        $myStatement->closeCursor();
+    } else {
+        ?>
+        <label>Aucun document ne correspond aux termes de recherche spécifiés.</label>
         <?php
     }
     $myStatement->closeCursor();
-} else {
-    $bdd = new DataBase();
-    $con = $bdd->getCon();
-
-    $queryEquipments = "SELECT DISTINCT(type_equip) FROM equipment ;";
-    $myStatement = $con->query($queryEquipments);
-
-    while ($donnees = $myStatement->fetch()) { ?>
-        <a href="Catalogue.php?type=<?php echo $donnees['type_equip'] ?>">
-            <div>
-                <strong> Type </strong> : <?php echo $donnees['type_equip']; ?> <br/>
-            </div>
-        </a>
-        <?php
-    }
-    $myStatement->closeCursor();
-
 }
+
+$myCatalogueController = new CatalogueController();
+$myCatalogueController->getEquipmentList();
 ?>
+
 
 <?php
 if (isset($_SESSION['isAdmin_user']) && $_SESSION['isAdmin_user'] == 1) {
@@ -88,5 +85,10 @@ if (isset($_SESSION['isAdmin_user']) && $_SESSION['isAdmin_user'] == 1) {
 }
 
 ?>
+
+</body>
+</html>
+
+
 </body>
 </html>
---
+++ code/RechercheUser.php
@@ -0,0 +1,55 @@
+<?php
+require_once("Controller/control-session.php");
+
+require "Controller/DataBase.php";
+
+?>
+<html>
+<body>
+<form method="POST" enctype="multipart/form-data">
+    <h1>Recherche utilisateur</h1>
+    <label>Rechercher utilisateur:</label>
+    <input type="search" placeholder="Matricule de l'utilisateur" name="UserToSearch">
+    <button type="submit" name="startSearching">Rechercher</button>
+</form>
+<?php
+
+if (isset($_POST['startSearching']) && $_POST['UserToSearch']!=null && $_POST['UserToSearch']!=" ") {
+    $bdd = new DataBase();
+    $con = $bdd->getCon();
+    $UserToSearch = trim($_POST['UserToSearch']);
+
+    $queryEquipments = "SELECT * FROM users WHERE matricule_user LIKE ?;";
+    $myStatement = $con->prepare($queryEquipments);
+    $myStatement->execute([$UserToSearch."%"]);
+
+    if($myStatement->rowCount() > 0)
+    {
+        while ($donnees = $myStatement->fetch()) { ?>
+            <a href="DetailUser.php?id_user_toDisplay=<?php echo $donnees['id_user'] ?>">
+                <div>
+                    <strong> Matricule </strong> : <?php echo $donnees['matricule_user']; ?> <br/>
+                    <strong> Firstname </strong> : <?php echo $donnees['name_user']; ?> <br/>
+                    <strong> Lastname </strong> : <?php echo $donnees['lastname_user']; ?> <br/>
+                    <strong> email </strong> : <?php echo $donnees['email_user']; ?> <br/> <br/>
+                </div>
+            </a>
+            <?php
+        }
+    }
+    else{
+        ?>
+        <label>Aucun document ne correspond aux termes de recherche spécifiés.</label>
+        <?php
+    }
+    $myStatement->closeCursor();
+}
+else{
+    ?>
+    <label>Veuillez remplir le champ de recherche avant d'appuyer sur le bouton.</label>
+    <?php
+}
+?>
+
+</body>
+</html>
