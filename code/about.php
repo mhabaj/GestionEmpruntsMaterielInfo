e4b2c14c514d983222862e337fe4f45fe3e91904
HDSean <sean.anica@etu.univ-tours.fr>
2020-12-26 21:04:24 +0100

    Create about.php

---
+++ code/about.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
+	
+	<title>About - Progressus Bootstrap template</title>
+
+	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
+	
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="assets/css/main.css">
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				<a class="navbar-brand" href="index.php"><img src="assets/images/logo.png" alt="Progressus HTML5 template"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li><a href="index.php">Home</a></li>
+					<li class="active"><a href="about.php">About</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a class="btn" href="Authentification.php">SIGN IN</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div> 
+	<!-- /.navbar -->
+
+	<header id="head" class="secondary"></header>
+
+	<!-- container -->
+	<div class="container">
+
+		<ol class="breadcrumb">
+			<li><a href="index.html">Home</a></li>
+			<li class="active">About</li>
+		</ol>
+
+		<div class="row">
+			
+			<!-- Article main content -->
+			<article class="col-sm-8 maincontent">
+				<header class="page-header">
+					<h1 class="page-title">About us</h1>
+				</header>
+				<h3>Objectifs du projet</h3>
+				<p><img src="assets/images/mac.jpg" alt="" class="img-rounded pull-right" width="200" > Projet de qualité logiciel en HTML/PHP du groupe 5. Le but de ce projet est de savoir utiliser les outils de tests unitaires du monde professionnel. Pour ce faire
+                nous avons commencés les combats de rue à l'âge de 3 ans pour pouvoir nous nourrir, puis, abandonnés dans la forêt par nos parents car ils n'avaient pas les moyens de s'occuper de nous, nous avons survécu au froid, à l'abandon et à la faim.
+                Nous nous soutenions entre nous pour ne pas sombrer dans un desespoir sans fin, à nos 6 ans nous avons été embusqués par un ours féroce, au bord de la mort... Nous avons révéillés la colère des Ackerman qui sommeillait en nous et avons déchirés la gorge de l'ours avec nos dents.
+                Sa peau et sa graisse nous ont permis de nous tenir au chaud dans notre caverne en hiver.</p>
+				<br><br><br><br>
+                <p>Groupe 5 : Alhabaj Mahmod, Ingarao Adrien, Belda Tom, Anica Sean, Ung Alexandre.</p>
+				
+			</article>
+			<!-- /Article -->
+
+		</div>
+	</div>	<!-- /container -->
+
+
+
+    <footer id="footer" class="top-space">
+
+        <div class="footer1">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-3 widget">
+                        <h3 class="widget-title">Contact</h3>
+                        <div class="widget-body">
+                            <p>numéro de téléphone au hasard<br>
+                                <a href="mailto:#">unemail@email.com</a><br>
+                                <br>
+                                Polytech'Tours
+                            </p>
+                        </div>
+                    </div>
+                </div> <!-- /row of widgets -->
+            </div>
+        </div>
+
+        <div class="footer2">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6 widget">
+                        <div class="widget-body">
+                            <p class="simplenav">
+                                <a href="#">Home</a> |
+                                <a href="about.php">About</a> |
+                                <a href="contact.php">Contact</a> |
+                                <b><a href="Authentification.php">Sign in</a></b>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 widget">
+                        <div class="widget-body">
+                            <p class="text-right">
+                                Copyright &copy; 2020, DI4 Groupe 5, Qualité logiciel.
+                            </p>
+                        </div>
+                    </div>
+
+                </div> <!-- /row of widgets -->
+            </div>
+        </div>
+
+    </footer>
+
+
+    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/headroom.min.js"></script>
+	<script src="assets/js/jQuery.headroom.min.js"></script>
+	<script src="assets/js/template.js"></script>
+</body>
+</html>
