80852b9a2ff4a78a539b300b7fa766403a589626
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2020-12-27 17:38:44 +0100

    merge Catalogue et RechercheUser

--- code/Catalogue.php
+++ code/Catalogue.php
@@ -69,18 +69,55 @@ if (isset($_SESSION['isAdmin_user']) && $_SESSION['isAdmin_user'] == 1) {
     $currentUser->loadUser();
     if ($currentUser->getPrivilege() == 1) {
         ?>
-
+        <h3>Espace Adminisatateur :</h3>
         <form method="POST" enctype="multipart/form-data">
-            <label>Espace Admin: </label>
             <button type="submit" name="addEquip">Ajouter un nouvel Equipement</button>
         </form>
-
+        <form method="POST" enctype="multipart/form-data">
+            <label>Rechercher utilisateur:</label>
+            <input type="search" placeholder="Matricule de l'utilisateur" name="UserToSearch">
+            <button type="submit" name="startSearchingUser">Rechercher</button>
+        </form>
         <?php
         if (isset($_POST['addEquip'])) {
             header("Location: creationEquipement.php");
         }
+    }
 
+    if (isset($_POST['startSearchingUser']) && $_POST['UserToSearch']!=null && $_POST['UserToSearch']!=" ") {
+        $bdd = new DataBase();
+        $con = $bdd->getCon();
+        $UserToSearch = trim($_POST['UserToSearch']);
 
+        $queryEquipments = "SELECT * FROM users WHERE matricule_user LIKE ?;";
+        $myStatement = $con->prepare($queryEquipments);
+        $myStatement->execute([$UserToSearch."%"]);
+
+        if($myStatement->rowCount() > 0)
+        {
+            while ($donnees = $myStatement->fetch()) { ?>
+                <a href="DetailUser.php?id_user_toDisplay=<?php echo $donnees['id_user'] ?>">
+                    <div>
+                        <strong> Matricule </strong> : <?php echo $donnees['matricule_user']; ?> <br/>
+                        <strong> Firstname </strong> : <?php echo $donnees['name_user']; ?> <br/>
+                        <strong> Lastname </strong> : <?php echo $donnees['lastname_user']; ?> <br/>
+                        <strong> email </strong> : <?php echo $donnees['email_user']; ?> <br/> <br/>
+                    </div>
+                </a>
+                <?php
+            }
+        }
+        else{
+            ?>
+            <label>Aucun document ne correspond aux termes de recherche spécifiés.</label>
+            <?php
+        }
+        $myStatement->closeCursor();
+    }
+    else{
+        ?>
+        <label>Veuillez remplir le champ de recherche avant d'appuyer sur le bouton.</label>
+        <?php
     }
 }
 
