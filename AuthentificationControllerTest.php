4fe9b7d4c6e0a6217279dfb110615251c84be087
HDSean <sean.anica@etu.univ-tours.fr>
2020-12-31 01:11:31 +0100

    php unit fixing

--- AuthentificationControllerTest.php
+++ AuthentificationControllerTest.php
@@ -5,6 +5,7 @@ use PHPUnit\Framework\TestCase;
 
 class AuthentificationControllerTest extends TestCase
 {
+    /** @test **/
     public function someTesting(){
         $stack = [];
         $this->assertEmpty($stack);
