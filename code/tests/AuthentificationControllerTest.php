06648b17b612ec79b27757d73a968af8a29cb7cd
HDSean <sean.anica@etu.univ-tours.fr>
2020-12-31 08:57:11 +0100

    jenkins test

--- code/tests/AuthentificationControllerTest.php
+++ code/tests/AuthentificationControllerTest.php
@@ -8,6 +8,7 @@ class AuthentificationControllerTest extends TestCase
     /** @test **/
     public function someTesting(){
         $stack = [];
+        $stack = array(1 => "a");
         $this->assertEmpty($stack);
 
         return $stack;
