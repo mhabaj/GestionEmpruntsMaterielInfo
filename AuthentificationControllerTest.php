fb26fb588716c891817a913938bc54a31c155384
HDSean <sean.anica@etu.univ-tours.fr>
2020-12-31 01:06:48 +0100

    unit testing adding test

---
+++ AuthentificationControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class AuthentificationControllerTest extends TestCase
+{
+    public function someTesting(){
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+}
