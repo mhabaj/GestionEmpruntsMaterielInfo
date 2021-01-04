cd692571a0ff006b5a3479a1567956dbfb79539b
ALHABAJ Mahmod <mhabaj99@hotmail.com>
2021-01-04 19:20:14 +0200

    wtf

--- code/DetailUser.php
+++ code/DetailUser.php
@@ -1,31 +1,44 @@
 <?php
-
 require_once("Controller/control-session.php");
 require_once("Controller/DataBase.php");
 require_once("Model/User.php");
 require_once("Model/UserRegular.php");
 require_once("Model/UserAdmin.php");
+<<<<<<< HEAD
 require_once("Controller/UserController.php");
 
 
 if (isset($_SESSION['isAdmin_user']) && $_SESSION['isAdmin_user'] == 1 && isset($_GET['id_user_toDisplay']) || isset($_GET['id_user_toDisplay']) && $_GET['id_user_toDisplay'] == $_SESSION['id_user'] )
 {
     $userController = new UserController();
+=======
+>>>>>>> parent of 730364f... MERGE AVEC ADRIEN ET ALEX
 
-    try
-    {
-        $userController->initUserController($_GET['id_user_toDisplay']);
-    }
-    catch (Exception $e)
-    {
-        echo $e->getMessage();
-        echo $_GET['id_user_toDisplay'];
+if (isset($_SESSION['isAdmin_user']) && $_SESSION['isAdmin_user'] == 1 || $_GET['id_user_toDisplay'] == $_SESSION['id_user']) {
+    ?>
+    <html>
+<body>
+    <?php
+    if (isset($_GET['id_user_toDisplay'])) {
+        try {
+            $currentUser = new UserRegular();
+            $currentUser->loadingUser($_GET['id_user_toDisplay']);
+        } catch (PDOException $e) {
+            throw new PDOException("Error!: " . $e->getMessage());
+        }
+        ?>
 
-        //header('Location: Catalogue.php');
-    }
+        <h2>Informations sur l'utilisateur </h2>
 
-    $currentUser = $userController->getUser();
+        <div>
+        <p> Nom : <?php echo $currentUser->getLastName(); ?> </p>
+        <p> Prénom : <?php echo $currentUser->getName(); ?> </p>
+        <p> Mail : <?php echo $currentUser->getEmail(); ?> </p>
+        <p> Identifiant de connexion : <?php echo $currentUser->getMatriculeUser(); ?> </p>
+        <p> numéro ID : <?php echo $currentUser->getIdUser(); ?> </p>
+        <br/>
 
+<<<<<<< HEAD
     if (isset($userController) && $userController != null && isset($currentUser) && $currentUser != null)
     {
     ?>
@@ -52,3 +65,33 @@ else
     header("refresh:3;url=Catalogue.php");
     echo "<p> Redirection dans 3 secondes.. </p>";
 }
+=======
+        <h3> Liste des emprunts courants de l'utilisateurs </h3>
+        <?php foreach ($currentUser->getBorrowList() as $borrowedItem) {
+            ?>
+            <p> Emprunt numero : <?php echo $borrowedItem->getIdBorrow(); ?></p>
+            <p> Reference de l'équipement emprunté : <?php echo $borrowedItem->getRefEquip(); ?> </p>
+            <p> Id du matériel physique preté : <?php echo $borrowedItem->getDeviceId(); ?> </p>
+            <p> Date de debut de l'emprunt : <?php echo $borrowedItem->getStartDate(); ?> </p>
+            <p> Date de fin de l'emprunt : <?php echo $borrowedItem->getEndDate(); ?></p>
+            <br/>
+            </div>
+            <?php
+        }
+        /* l'utilisateur et l'admin peuvent acceder à l'historique */
+
+        /* seulement l'admin peut acceder à la modification de user */
+        if ($_SESSION['isAdmin_user'] == 1) {
+            ?>
+            <a href="ModifyUser.php?id_user_toDisplay=<?php echo $_GET['id_user_toDisplay'] ?>"
+               title="Modifier l'utilisateur">Modifier l'utilisateur</a>
+            </body>
+            </html>
+            <?php
+        }
+    } else
+        echo "Erreur, impossible de charger l'utilisateur";
+} else
+    echo "Vous n'avez pas accès à cette page"
+?>
+>>>>>>> parent of 730364f... MERGE AVEC ADRIEN ET ALEX
--- code/Model/User.php
+++ code/Model/User.php
@@ -14,6 +14,8 @@ abstract class User
     protected $_lastName;
     protected $_phone;
     protected $_privilege;
+
+
     protected $_borrowList = array();
 
     /**
@@ -34,8 +36,127 @@ abstract class User
 
     public function delBorrowToList($idToDel)
     {
+<<<<<<< HEAD
         unset($this->_borrowList[$idToDel]);
     }
+=======
+        $bdd = new DataBase();
+        $con = $bdd->getCon();
+        $query = "SELECT * FROM users WHERE id_user = ? ;";
+        $stmt = $con->prepare($query);
+        $stmt->execute([$id_userToLoad]);
+
+        $result = $stmt->fetch();
+        $this->setIdUser($result['id_user']);
+        $this->setEmail($result['email_user']);
+        $this->setMatriculeUser($result['matricule_user']);
+        $this->setPassword($result['password_user']);
+        $this->setName($result['name_user']);
+        $this->setLastName($result['lastname_user']);
+        $this->setPhone($result['phone_user']);
+        $this->setPrivilege($result['isAdmin_user']);
+
+
+        $myQuery = "SELECT borrow_info.id_borrow,startdate_borrow,enddate_borrow,isActive, borrow.id_device, device.ref_equip FROM borrow_info
+                    INNER JOIN borrow INNER JOIN device ON borrow.id_borrow= borrow_info.id_borrow AND borrow.id_device= device.id_device
+                    WHERE borrow.id_user = '$this->_idUser' and borrow_info.isActive = 1;";
+        $myStatement = $con->query($myQuery);
+        $result = $myStatement->rowCount();
+        $borrowLignes = $myStatement->fetchAll();
+
+        if ($result == 0)
+            $this->_borrowList = array();
+        else {
+            foreach ($borrowLignes as $borrow) {
+                $BorrowItem = new Borrow($borrow['ref_equip'], $borrow['enddate_borrow']);
+                $BorrowItem->setStartDate($borrow['startdate_borrow']);
+                $BorrowItem->setDeviceId($borrow['id_device']);
+                $BorrowItem->setIdBorrow($borrow['id_borrow']);
+                array_push($this->_borrowList, $BorrowItem);
+            }
+        }
+        $bdd->closeCon();
+    }
+
+
+    public function loadUser()
+    {
+
+        $this->_idUser = $_SESSION['id_user'];
+
+        $bdd = new DataBase();
+        $con = $bdd->getCon();
+        $query = "SELECT * FROM users WHERE id_user = ? ;";
+        $stmt = $con->prepare($query);
+        $stmt->execute([$this->_idUser]);
+        $result = $stmt->fetch();
+        $this->setEmail($result['email_user']);
+        $this->setMatriculeUser($result['matricule_user']);
+        $this->setPassword($result['password_user']);
+        $this->setName($result['name_user']);
+        $this->setLastName($result['lastname_user']);
+        $this->setPhone($result['phone_user']);
+        $this->setPrivilege($result['isAdmin_user']);
+
+        $myQuery = "SELECT borrow_info.id_borrow,startdate_borrow,enddate_borrow,isActive, borrow.id_device, device.ref_equip FROM borrow_info 
+                    INNER JOIN borrow INNER JOIN device ON borrow.id_borrow= borrow_info.id_borrow AND borrow.id_device= device.id_device
+                    WHERE borrow.id_user = '$this->_idUser' and borrow_info.isActive = 1;";
+        $myStatement = $con->query($myQuery);
+        $result = $myStatement->rowCount();
+        $borrowLignes = $myStatement->fetchAll();
+
+        if ($result == 0)
+            $this->_borrowList = array();
+        else {
+            foreach ($borrowLignes as $borrow) {
+                $BorrowItem = new Borrow($borrow['ref_equip'], $borrow['enddate_borrow']);
+                $BorrowItem->setStartDate($borrow['startdate_borrow']);
+                $BorrowItem->setDeviceId($borrow['id_device']);
+                $BorrowItem->setIdBorrow($borrow['id_borrow']);
+                array_push($this->_borrowList, $BorrowItem);
+            }
+        }
+        $bdd->closeCon();
+    }
+
+    public function connect()
+    {
+
+        $bdd = new DataBase();
+        $con = $bdd->getCon();
+
+        //Hashage du mdp
+        $hash_mdp = sha1($this->_password);
+
+        //Inserer valeurs
+        $requete = "SELECT * FROM users WHERE matricule_user = ? AND password_user= ? ;";
+        $stmt = $con->prepare($requete);
+        $stmt->execute([$this->_matriculeUser, $hash_mdp]);
+        $result = $stmt->rowCount();
+
+        if ($result == 1) {
+            session_start();
+            $infoUser = $stmt->fetch();
+            $_SESSION['id_user'] = $infoUser['id_user'];
+            $this->_idUser = $infoUser['id_user'];
+            $this->loadUser();
+            $bdd->closeCon();
+            return TRUE;
+        } else {
+            $bdd->closeCon();
+            return FALSE;
+        }
+    }
+
+    public function disconnect()
+    {
+        session_unset();
+        session_destroy();
+        return TRUE;
+    }
+
+
+>>>>>>> parent of 730364f... MERGE AVEC ADRIEN ET ALEX
     /**
      * @return mixed
      */
@@ -163,6 +284,7 @@ abstract class User
     {
         $this->_borrowList = $borrowList;
     }
+
     /**
      * @return mixed
      */
@@ -178,8 +300,11 @@ abstract class User
     {
         $this->_privilege = $privilege;
     }
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> parent of 730364f... MERGE AVEC ADRIEN ET ALEX
 }
 /*
 $user = new User();
