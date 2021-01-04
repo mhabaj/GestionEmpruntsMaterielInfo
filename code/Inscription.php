5f834cc4c619f1c817d82f236a945df4ea3c3777
Adrien <adrien.ingand@gmail.com>
2021-01-04 15:46:42 +0100

    updates css
    add css to authentification/catlaogue

--- code/Inscription.php
+++ code/Inscription.php
@@ -5,53 +5,98 @@ require_once("Controller/DataBase.php");
 require_once("Model/User.php");
 require_once("Model/UserRegular.php");
 require_once("Model/UserAdmin.php");
-require_once("Controller/InscriptionController.php");
 
 /* catch les erreurs */
 if(isset($_SESSION['isAdmin_user']) && $_SESSION['isAdmin_user'] == 1)
 {
+    $currentUser = new UserAdmin();
+    $currentUser->loadUser();
+    if(isset($_POST['submitInscription'])){
 
-    //on include inscriptions.view
-    require_once('view/inscripition.view.php');
-    ?>
+        $matricule = $_POST['matricule'];
+        $password = $_POST['password'];
+        $passwordrepeat = $_POST['passwordrepeat'];
+        $email = $_POST['email'];
+        $lastname = $_POST['lastname'];
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $isAdmin =0;
+        if(isset($_POST['administrateur']))
+        {
+            $isAdmin = $_POST['administrateur'];
+            if ($isAdmin == 'ok')
+            {
+                $isAdmin = 1;
+            }
+        }
 
-    <?php
-    /* si l'admin décide d'annuler sa création d'utilisateur */
-    if(isset($_POST['cancelbtn']))
-    {
+        if(strlen($matricule) == 7 && ($password == $passwordrepeat))
+        {
+            try
+            {
+                $currentUser->createUser($matricule,$email,$password,$name,$lastname,$phone,$isAdmin);
+            }
+            catch(Exception $e)
+            {
+                echo $e->getMessage();
+            }
+        }
+        else
+        {
+            echo "<p> Erreur, le matricule doit comporter 7 caracteres et/ou les mots de passe entrés ne correspondent pas <p/>";
+        }
+    }
+    if(isset($_POST['cancelbtn'])){
         header('Location: Catalogue');
     }
 
-    /* si l'admin décide de créer un utilisateur*/
-    if(isset($_POST['submitInscription']))
-    {
-        try
-        {
-            $myInscriptionController = new InscriptionController();
-            $myInscriptionController->initInscriptionController();
+    ?>
 
-            if(isset($_POST['administrateur']) && isset($_POST['matricule']) && isset($_POST['password']) && isset($_POST['passwordrepeat']) && isset($_POST['name']) && isset($_POST['lastname'])
-                && isset($_POST['phone']) && isset($_POST['administrateur']))
-            {
-                if($myInscriptionController->createUserController($_POST['matricule'],$_POST['password'],$_POST['passwordrepeat'],$_POST['email'],$_POST['name'],$_POST['lastname'],$_POST['phone'],$_POST['administrateur']) == true)
-                {
-                    echo "L'utilisateur a correctement été créé";
-                    header( "refresh:3;url=Inscription.php");
-                }
-            }
-        }
-        catch(Exception $e)
-        {
-            echo $e->getMessage();
-            header( "refresh:3;url=Inscription.php");
-        }
+    <html lang="fr">
 
-    }
+    <body>
 
-}
-else
-{
-    echo "Vous n'avez pas accès à cette page, vous allez être redirigé";
-    header( "refresh:2;url=Catalogue.php");
+    <form method="POST" enctype="multipart/form-data">
+        <h1>Inscrire un utilisateur</h1>
+        <p>Veuillez remplir les champs ci-dessous pour inscrire un utilisateur</p>
+        <hr>
+
+        <label><b>Nom d'utilisateur</b></label>
+        <input type="text" placeholder="Entrer le nom d'utilisateur" name="matricule" required>
+        <br><br>
+        <label><b>Mot de passe</b></label>
+        <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+        <label><b>Retapez le mot de passe</b></label>
+        <input type="password" placeholder="Répéter le mot de passe" name="passwordrepeat" required>
+        <br><br>
+        <label><b>Email</b></label>
+        <input type="email" placeholder="Entrez l'adresse mail de l'utilisateur" name="email" required>
+        <br><br>
+        <label><b>nom de famille</b></label>
+        <input type="text" placeholder="Entrez le nom de famille" name="lastname" required>
+
+        <label><b>prénom</b></label>
+        <input type="text" placeholder="Entrez le prénom" name="name" required>
+        <br><br>
+        <label><b>numéro de téléphone</b></label>
+        <input type="tel" pattern="[0-9]{10}" placeholder="Entrez le numéro de téléphone" name="phone" required>
+        <br><br>
+        <label><b>Confirmez si l'utilisateur sera un administrateur</b></label>
+        <label>
+            <input type="checkbox" checked="checked" name="administrateur"  value ="ok" style="margin-bottom:15px">Administrateur
+        </label>
+
+        <hr>
+        <button type="button" name="cancelbtn">Annuler l'inscription</button>
+        <button type="submit" name="submitInscription">Confirmer l'inscription</button>
+
+    </form>
+
+    </body>
+
+    </html>
+
+    <?php
 }
 ?>
--- code/view/modifierEquipement.view.php
+++ code/view/modifierEquipement.view.php
@@ -1,31 +1,31 @@
 <html>
-<body>
-<form method="POST" enctype="multipart/form-data">
-    <label>Modifier cette équipement: </label>
-    <br>
-    <p>Reference d'équipement: </p>
+    <body>
+        <form method="POST" enctype="multipart/form-data">
+            <label>Modifier cette équipement: </label>
+            <br>
+            <p>Reference d'équipement: </p>
 
-    <input type="text" placeholder="Reference de l'équipement" name="ref_equip" required
-           value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getRefEquip(); ?>">
-    <p>Type d'équipement: </p>
-    <input type="text" placeholder="Type de l'équipement" name="type_equip" required
-           value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getTypeEquip(); ?>">
-    <p> Nom du materiel: </p>
-    <input type="text" placeholder="Nom de l'équipement" name="nom_equip" required
-           value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getNameEquip() ?>">
-    <p>Marque: </p>
-    <input type="text" placeholder="Marque de l'équipement" name="marque_equip" required
-           value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getBrandEquip(); ?>">
-    <p> Version du materiel: </p>
-    <input type="text" placeholder="Version de l'équipement" name="version_equip" required
-           value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getVersionEquip(); ?>">
-    <p> Quantité totale du materiel <b>(Disponible à modifier/ Occupé / Total présent:
-            <mark><?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo MainDAO::howMuchAvailable($currentEquipement->getRefEquip()) . "/" . (MainDAO::howMuchAvailable($currentEquipement->getRefEquip()) - MainDAO::howMuchTotal($currentEquipement->getRefEquip())) * -1 . "/" . MainDAO::howMuchTotal($currentEquipement->getRefEquip()); ?></mark>
-            ): </b></p>
-    <input type="number" placeholder="Quantite de l'équipement" name="quantite_equip" required
-           min="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo(MainDAO::howMuchTotal($currentEquipement->getRefEquip()) - MainDAO::howMuchAvailable($currentEquipement->getRefEquip())); ?>"
-           value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo MainDAO::howMuchTotal($currentEquipement->getRefEquip()); ?>">
+            <input type="text" placeholder="Reference de l'équipement" name="ref_equip" required
+                   value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getRefEquip(); ?>">
+            <p>Type d'équipement: </p>
+            <input type="text" placeholder="Type de l'équipement" name="type_equip" required
+                   value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getTypeEquip(); ?>">
+            <p> Nom du materiel: </p>
+            <input type="text" placeholder="Nom de l'équipement" name="nom_equip" required
+                   value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getNameEquip() ?>">
+            <p>Marque: </p>
+            <input type="text" placeholder="Marque de l'équipement" name="marque_equip" required
+                   value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getBrandEquip(); ?>">
+            <p> Version du materiel: </p>
+            <input type="text" placeholder="Version de l'équipement" name="version_equip" required
+                   value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo $currentEquipement->getVersionEquip(); ?>">
+            <p> Quantité totale du materiel <b>(Disponible à modifier/ Occupé / Total présent:
+                    <mark><?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo MainDAO::howMuchAvailable($currentEquipement->getRefEquip()) . "/" . (MainDAO::howMuchAvailable($currentEquipement->getRefEquip()) - MainDAO::howMuchTotal($currentEquipement->getRefEquip())) * -1 . "/" . MainDAO::howMuchTotal($currentEquipement->getRefEquip()); ?></mark>
+                    ): </b></p>
+            <input type="number" placeholder="Quantite de l'équipement" name="quantite_equip" required
+                   min="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo(MainDAO::howMuchTotal($currentEquipement->getRefEquip()) - MainDAO::howMuchAvailable($currentEquipement->getRefEquip())); ?>"
+                   value="<?php if (isset($EquipmentController) && $EquipmentController != null && isset($currentEquipement) && $currentEquipement != null) echo MainDAO::howMuchTotal($currentEquipement->getRefEquip()); ?>">
 
-    <input type="submit" value="Modifier Valeur" placeholder="Modifier l'équipement"
-           name="modifierEquipment">
-</form>
+            <input type="submit" value="Modifier Valeur" placeholder="Modifier l'équipement"
+                   name="modifierEquipment">
+        </form>
