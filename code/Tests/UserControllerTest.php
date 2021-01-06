d16b9be2c809436dea9a5591493fb833b60b1044
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2021-01-06 23:38:44 +0100

    Update UserControllerTest.php

--- code/Tests/UserControllerTest.php
+++ code/Tests/UserControllerTest.php
@@ -1,9 +1,37 @@
 <?php
 
+require_once(__DIR__ ."/../Controller/EquipmentController.php");
+require_once(__DIR__ ."/../Model/Equipment.php");
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class UserControllerTest
+ * @covers UserController
+ */
 class UserControllerTest extends TestCase
 {
+    /**
+     * @covers UserController::startBorrow
+     * @dataProvider providerInvalidRef
+     * @param String $refEquip
+     * @throws Exception
+     */
+    public function startBorrow($equipmentController, $ref_equip_toBorrow, $dateFin, $quantity)
+    {
 
+    }
+
+
+    public function providerInvalidRef(): array
+    {
+        return [
+            ["A0"],
+            ["AAAAA"],
+            ["00000"],
+            [0],
+            [0.111],
+            [1000000]
+        ];
+    }
 }
