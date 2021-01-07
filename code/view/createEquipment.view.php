23bbca8c2c16e100991880d07cb7e549a6622ed3
HDSean <sean.anica@etu.univ-tours.fr>
2021-01-07 16:06:08 +0100

    gg

--- code/view/createEquipment.view.php
+++ code/view/createEquipment.view.php
@@ -14,46 +14,69 @@ require_once("navbar.view.php");
 <form method="POST" enctype="multipart/form-data">
     <h1>Création matériel</h1>
 
-    <label><b>Référence</b></label>
-    <input type="text" placeholder="Référence matériel" name="ref"
-           value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
-               echo $currentEquipment->getRefEquip(); ?>"
-           required>
+    <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm"><label><b>Référence</b></label></span>
+        </div>
+        <input class="form-control" aria-label="Small"  type="text" placeholder="Référence matériel" name="ref"
+               value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
+                   echo $currentEquipment->getRefEquip(); ?>"
+               required>
+    </div>
 
-    <br/><label><b>Type</b></label>
-    <input type="text" placeholder="Type matériel" name="type"
-           value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
-               echo $currentEquipment->getTypeEquip(); ?>"
-           required>
+    <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm"><br/><label><b>Type</b></label></span>
+        </div>
+        <input class="form-control" aria-label="Small"  placeholder="Type matériel" name="type"
+               value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
+                   echo $currentEquipment->getTypeEquip(); ?>"
+               required>
+    </div>
 
-    <br/><label><b>Marque</b></label>
-    <input type="text" placeholder="Marque matériel" name="brand"
-           value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
-               echo $currentEquipment->getBrandEquip(); ?>"
-           required>
+    <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm"><br/><label><b>Marque</b></label></span>
+        </div>
+        <input class="form-control" aria-label="Small"  type="text" placeholder="Marque matériel" name="brand"
+               value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
+                   echo $currentEquipment->getBrandEquip(); ?>"
+               required>
+    </div>
 
-    <br/><label><b>Nom</b></label>
-    <input type="text" placeholder="Nom matériel" name="name"
-           value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
-               echo $currentEquipment->getNameEquip(); ?>"
-           required>
+    <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm"><br/><label><b>Nom</b></label></span>
+        </div>
+        <input class="form-control" aria-label="Small"  type="text" placeholder="Nom matériel" name="name"
+               value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
+                   echo $currentEquipment->getNameEquip(); ?>"
+               required>
+    </div>
 
-    <br/><label><b>Version</b></label>
-    <input type="text" placeholder="Version matériel" name="version"
-           value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
-               echo $currentEquipment->getVersionEquip(); ?>" required>
+    <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm"><br/><label><b>Version</b></label></span>
+        </div>
+        <input class="form-control" aria-label="Small"  type="text" placeholder="Version matériel" name="version"
+               value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
+                   echo $currentEquipment->getVersionEquip(); ?>" required>
+    </div>
 
-    <br/><label><b>Quantité</b></label>
-    <input type="number" placeholder="Quantité matériel" name="quantity"
-           value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
-               if (isset($quantity) && $quantity != null) echo $quantity; ?>" required>
-
+    <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm"><br/><label><b>Quantité</b></label></span>
+        </div>
+        <input class="form-control" aria-label="Small"  type="number" placeholder="Quantité matériel" name="quantity"
+               value="<?php if (isset($equipmentController) && $equipmentController != null && isset($currentEquipment) && $currentEquipment != null)
+                   if (isset($quantity) && $quantity != null) echo $quantity; ?>" required>
+    </div>
 
     <br/><label><b>Images: </b></label>
     <input type="image" max="1024" value="" name="photo">
     <input type="hidden" name="MAX_FILE_SIZE" value="8388608">
     <label for="photo"> Photo (JPG, JPEG, PNG or GIF | max.8 Mo) :</label><br/>
-    <input type="file" id="photo" name="photo"/><br/>
+    <input class="form-control" type="file" id="photo" name="photo"/><br/>
 
 
 
