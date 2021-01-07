3c39813512f3656fd2e55a82dbc4e116b82ee5a5
HDSean <sean.anica@etu.univ-tours.fr>
2021-01-07 17:05:12 +0100

    css update

--- code/view/buttondetailUser.view.php
+++ code/view/buttondetailUser.view.php
@@ -1,10 +1,3 @@
 <?php
 require_once("dashboard.view.php");
 ?>
-
-<form method="get" action="DetailUser.php">
-    <button type="submit"
-            value="<?php if (isset($_SESSION['id_user']) && $_SESSION['id_user'] != "") echo $_SESSION['id_user'] ?>"
-            name="id_user_toDisplay">Mon compte
-    </button>
-</form>
