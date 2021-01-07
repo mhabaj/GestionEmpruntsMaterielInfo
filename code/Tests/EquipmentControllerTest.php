bc956478803e97e5427a9ad8904454236d66a701
HDSean <sean.anica@etu.univ-tours.fr>
2021-01-07 16:33:36 +0100

    test update

--- code/Tests/EquipmentControllerTest.php
+++ code/Tests/EquipmentControllerTest.php
@@ -3,6 +3,7 @@
 require_once(__DIR__ ."/../Controller/EquipmentController.php");
 require_once(__DIR__ ."/../Controller/DataBase.php");
 require_once(__DIR__ ."/../Model/Equipment.php");
+require_once(__DIR__ ."/../ControllerDAO/EquipmentDAO.php");
 
 use PHPUnit\Framework\TestCase;
 
@@ -12,223 +13,150 @@ use PHPUnit\Framework\TestCase;
  */
 final class EquipmentControllerTest extends TestCase
 {
-    /**
-     * @covers EquipmentController::initEquipmentController
-     * @dataProvider providerInvalidRef
-     * @param String $refEquip
-     * @throws Exception
-     */
-    public function testInitInvalid($refEquip): void
+
+    public static function deleteEquipment($ref){
+        $bdd = new DataBase();
+        $con = $bdd->getCon();
+
+        $queryEquipments = "DELETE FROM stock_photo WHERE stock_photo.ref_equip LIKE ?;";
+        $myStatement = $con->prepare($queryEquipments);
+        $myStatement->execute([$ref]);
+
+        $queryEquipments = "DELETE FROM device WHERE device.ref_equip LIKE ?;";
+        $myStatement = $con->prepare($queryEquipments);
+        $myStatement->execute([$ref]);
+
+        $queryEquipments = "DELETE FROM equipment WHERE equipment.ref_equip LIKE ?;";
+        $myStatement = $con->prepare($queryEquipments);
+        $myStatement->execute([$ref]);
+
+        $myStatement->closeCursor();
+        $bdd->closeCon();
+    }
+
+    public static function setUpBeforeClass(): void
     {
+        $edao = new EquipmentDAO();
+
+        $edao->createEquipment("XX001", "TypeEquip", "BrandEquip", "NameEquip", "VersionEquip", 1);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        EquipmentControllerTest::deleteEquipment("XX000");
+        EquipmentControllerTest::deleteEquipment("XX001");
+        EquipmentControllerTest::deleteEquipment("XX002");
+        EquipmentControllerTest::deleteEquipment("XX003");
+        EquipmentControllerTest::deleteEquipment("XX004");
+    }
+
+    public function testCreateEquipment(): void
+    {
+        $equipment1 = new Equipment("XX000", "TypeTest", "NameTest", "BrandTest", "VersionTest");
+
         $ec = new EquipmentController();
+
+        $ec->loadEquipmentFromObject($equipment1);
+
+        $ec->createNewEquipment(3);
+
+        $ec->loadEquipmentFromDDB("XX000");
+
+        $this->assertEquals(3, $ec->getEquipmentDAO()->howMuchTotal("XX000"));
+
+        $equipment2 = $ec->getEquipment();
+
+        $this->assertEquals($equipment1->getRefEquip(), $equipment2->getRefEquip());
+        $this->assertEquals($equipment1->getBrandEquip(), $equipment2->getBrandEquip());
+        $this->assertEquals($equipment1->getVersionEquip(), $equipment2->getVersionEquip());
+        $this->assertEquals($equipment1->getTypeEquip(), $equipment2->getTypeEquip());
+        $this->assertEquals($equipment1->getNameEquip(), $equipment2->getNameEquip());
+
+        $this->deleteEquipment("XX000");
+    }
+
+    public function testCreateEquipmentDoublon(): void
+    {
+        $equipment = new Equipment("XX000", "TypeTest", "NameTest", "BrandTest", "VersionTest");
+
+        $ec = new EquipmentController();
+
+        $ec->loadEquipmentFromObject($equipment);
+
+        $ec->createNewEquipment(3);
 
         $this->expectException(Exception::class);
 
-        $ec->initEquipmentController($refEquip);
+        $ec->createNewEquipment(1);
+
+        $this->deleteEquipment("XX000");
     }
 
-    /**
-     * @covers EquipmentController::initEquipmentController
-     * @throws Exception
-     */
-    public function testInitValid(): void
+    public function testModifyEquipment(): void
     {
         $ec = new EquipmentController();
 
-        $ec->initEquipmentController("XX000");
-
-        $e = $ec->getEquipment();
-
-        $this->assertEquals("XX000", $e->getRefEquip(), "Erreur ref");
-        $this->assertEquals("TypeTest", $e->getTypeEquip(), "Erreur type");
-        $this->assertEquals("BrandTest", $e->getBrandEquip(), "Erreur marque");
-        $this->assertEquals("NameTest", $e->getNameEquip(), "Erreur nom");
-        $this->assertEquals("VersionTest", $e->getVersionEquip(), "Erreur version");
-    }
-
-    /**
-     * @covers EquipmentController::modifyEquipment
-     * @depends testInitValid
-     * @dataProvider providerInvalidEquipmentNR
-     * @param $refEquip
-     * @param $type_equip
-     * @param $marque_equip
-     * @param $nom_equip
-     * @param $version_equip
-     * @param $quantite_equip
-     * @throws Exception
-     */
-    public function testModifyEquipmentInvalid($type_equip, $marque_equip, $nom_equip, $version_equip, $quantite_equip): void
-    {
-        $ec = new EquipmentController();
-
-        $ec->initEquipmentController("XX000");
-
-        $this->expectException(Exception::class);
-
-        $ec->modifyEquipment("XX000", $type_equip, $nom_equip, $marque_equip, $version_equip, $quantite_equip);
-    }
-
-    /**
-     * @covers  EquipmentController::modifyEquipment
-     * @depends testInitValid
-     * @throws Exception
-     */
-    public function testModifyEquipmentValid(): void
-    {
-        $_SESSION["id_user"]=3;
-
-        $ec = new EquipmentController();
-
-        $ec->initEquipmentController("XX001");
+        $ec->loadEquipmentFromDDB("XX001");
 
         $ec->modifyEquipment("XX001", "TypeTest1", "NameTest1", "BrandTest1", "VersionTest1", 1);
 
-        $ec->initEquipmentController("XX001");
+        $ec->loadEquipmentFromDDB("XX001");
 
-        $e = $ec->getEquipment();
+        $equipment = $ec->getEquipment();
 
-        $this->assertEquals("XX001", $e->getRefEquip(), "Erreur ref");
-        $this->assertEquals("TypeTest1", $e->getTypeEquip(), "Erreur type");
-        $this->assertEquals("BrandTest1", $e->getBrandEquip(), "Erreur marque");
-        $this->assertEquals("NameTest1", $e->getNameEquip(), "Erreur nom");
-        $this->assertEquals("VersionTest1", $e->getVersionEquip(), "Erreur version");
+        $this->assertEquals($equipment->getRefEquip(), "XX001");
+        $this->assertEquals($equipment->getBrandEquip(), "BrandTest1");
+        $this->assertEquals($equipment->getVersionEquip(), "VersionTest1");
+        $this->assertEquals($equipment->getTypeEquip(), "TypeTest1");
+        $this->assertEquals($equipment->getNameEquip(), "NameTest1");
 
-        $ec->modifyEquipment("XX001", "TypeTest", "NameTest", "BrandTest", "VersionTest", 1);
     }
 
-    /**
-     * @covers EquipmentController::isRefEquipUsed
-     * @dataProvider providerInvalidRef
-     * @param $ref
-     */
-    public function testIsRefEquipInvalid($ref): void
-    {
-        $ec = new EquipmentController();
-        $ec->initEquipmentController("XX000");
-
-        $this->assertFalse($ec->isRefEquipUsed($ref));
-    }
-
-    /**
-     * @covers EquipmentController::isNewRefEquipUsed
-     * @dataProvider providerInvalidRef
-     * @param $ref
-     */
-    public function testIsNewRefEquipInvalid($ref): void
+    public function testModifyEquipmentDoublon(): void
     {
         $ec = new EquipmentController();
 
-        $this->assertFalse($ec->isNewRefEquipUsed($ref));
-    }
+        $equipment1 = new Equipment("XX002", "TypeTest", "NameTest", "BrandTest", "VersionTest");
 
-    /**
-     * @covers EquipmentController::createNewEquipment
-     * @dataProvider providerInvalidEquipmentR
-     * @param $ref_equip
-     * @param $type_equip
-     * @param $marque_equip
-     * @param $nom_equip
-     * @param $version_equip
-     * @param $quantite_equip
-     * @throws Exception
-     */
-    public function testCreateNewEquipmentInvalid($ref_equip, $type_equip, $marque_equip, $nom_equip, $version_equip, $quantite_equip): void
-    {
-        $ec = new EquipmentController();
+        $ec->loadEquipmentFromObject($equipment1);
+
+        $ec->createNewEquipment(1);
+
+        $equipment2 = new Equipment("XX003", "TypeTest", "NameTest", "BrandTest", "VersionTest");
+
+        $ec->loadEquipmentFromObject($equipment2);
+
+        $ec->createNewEquipment(1);
 
         $this->expectException(Exception::class);
 
-        $ec->createNewEquipment($ref_equip, $type_equip, $nom_equip, $marque_equip, $version_equip, $quantite_equip);
+        $ec->modifyEquipment("XX002", "TypeTest1", "NameTest1", "BrandTest1", "VersionTest1", 1);
+
     }
 
-    /**
-     * @covers EquipmentController::reserveEquipment
-     * @dataProvider providerReserve
-     * @param $dateFinBorrow
-     * @param $quantite_equip
-     * @throws Exception
-     */
-    public function testReserveEquipmentInvalid($dateFinBorrow, $quantite_equip): void
+    public function testModifyEquipmentDeviceNumber(): void
     {
         $ec = new EquipmentController();
-        $ec->initEquipmentController("XX000");
+
+        $equipment = new Equipment("XX004", "TypeTest", "NameTest", "BrandTest", "VersionTest");
+
+        $ec->loadEquipmentFromObject($equipment);
+
+        $ec->createNewEquipment(3);
+
+        $this->assertEquals(3, $ec->getEquipmentDAO()->howMuchTotal("XX004"));
+
+        $ec->modifyEquipment("XX004", "TypeTest", "NameTest", "BrandTest", "VersionTest", 5);
+
+        $this->assertEquals(5, $ec->getEquipmentDAO()->howMuchTotal("XX004"));
+
+        $ec->modifyEquipment("XX004", "TypeTest", "NameTest", "BrandTest", "VersionTest", 1);
+
+        $this->assertEquals(1, $ec->getEquipmentDAO()->howMuchTotal("XX004"));
 
         $this->expectException(Exception::class);
 
-        $ec->reserveEquipment($dateFinBorrow, $quantite_equip);
-    }
-
-    /////////////////////////////////////////////////////////////////////////////////////
-    /// PROVIDER ////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////////////////
-
-    public function providerReserve(): array
-    {
-        $_SESSION["id_user"]=3;
-
-        return[
-            ["2100-01-01","0"],
-            [0,1],
-            ["00000",1],
-            ["2000-01-01",1],
-            ["2100-20-01",1],
-            ["2100-01-40",1],
-            ["2100-01-01",-1],
-        ];
-
-    }
-
-    public function providerInvalidEquipmentR(): array
-    {
-        $_SESSION["id_user"]=3;
-
-        return[
-            ["A0", "A", "A", "A", "A", 1],
-            ["AAAAA", "A", "A", "A", "A", 1],
-            ["00000", "A", "A", "A", "A", 1],
-            [0, "A", "A", "A", "A", 1],
-            [0.111, "A", "A", "A", "A", 1],
-            [1000000, "A", "A", "A", "A", 1],
-            ["XX002", "A", "A", "A", "A", -1],
-            ["XX002", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "A", "A", "A", 1],
-            ["XX002", 1, "A", "A", "A", 1],
-            ["XX002", "A", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "A", "A", 1],
-            ["XX002", "A", 1, "A", "A", 1],
-            ["XX002", "A", "A", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "A", 1],
-            ["XX002", "A", "A", 1, "A", 1],
-            ["XX002", "A", "A", "A", "AAAAAAAAAAAAAAAA", 1],
-            ["XX002", "A", "A", "A", 1, 1]
-        ];
-    }
-
-    public function providerInvalidEquipmentNR(): array
-    {
-        $_SESSION["id_user"]=3;
-
-        return[
-            ["A", "A", "A", "A", -1],
-            ["AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "A", "A", "A", 1],
-            [1, "A", "A", "A", 1],
-            ["A", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "A", "A", 1],
-            ["A", 1, "A", "A", 1],
-            ["A", "A", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "A", 1],
-            ["A", "A", 1, "A", 1],
-            ["A", "A", "A", "AAAAAAAAAAAAAAAA", 1],
-            ["A", "A", "A", 1, 1]
-        ];
-    }
-
-    public function providerInvalidRef(): array
-    {
-        return [
-            ["A0"],
-            ["AAAAA"],
-            ["00000"],
-            [0],
-            [0.111],
-            [1000000]
-        ];
+        $ec->modifyEquipment("XX004", "TypeTest", "NameTest", "BrandTest", "VersionTest", -1);
     }
 
 }
